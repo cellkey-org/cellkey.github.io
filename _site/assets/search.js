528266f44f08ea134fdfb4ef7632a92bbf95567e
daniel <129152133+cellkey-daniel@users.noreply.github.com>
2023-05-01 18:01:13 +0900

    fix: site update

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/2023/04/01/welcome.html"},{'title':"Post with Header Image",'url':"/2023/05/01/header-image.html"}]};
